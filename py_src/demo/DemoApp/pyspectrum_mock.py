021ab2cdd5663596d8cc5af0430448e5a334def3
Oleg Ivanov <o.ivanov@g.nsu.ru>
2022-11-10 00:05:13 +0700

    Spectrum drawed in PyQT

---
+++ py_src/demo/DemoApp/pyspectrum_mock.py
@@ -0,0 +1,70 @@
+import dataclasses
+import time
+from typing import Any
+import numpy as np
+from os import path
+import pickle
+
+
+
+class SpectrumInfo(object):
+    clipped: np.array
+    samples: np.array
+
+    @property
+    def n_times(self) -> int:
+        return self.samples.shape[0]
+
+    @property
+    def n_samples(self) -> int:
+        return self.samples.shape[1]
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        return self.samples.shape
+
+    def __repr__(self) -> str:
+        return  f'{type(self).__name__}({self.n_times = }, {self.n_samples = })'
+
+
+@dataclasses.dataclass(repr=False)
+class Spectrum(SpectrumInfo):
+    clipped: np.array
+    samples: np.array
+    wavelength: np.array
+
+
+class CustomUnpickler(pickle.Unpickler):
+    def find_class(self, __module_name: str, __global_name: str) -> Any:
+        if __global_name == 'Spectrum':
+            return Spectrum
+        return super().find_class(__module_name, __global_name)
+
+_data = CustomUnpickler(open(path.join(path.dirname(path.realpath(__file__)), 'data.pickle'), 'rb')).load()
+
+
+def __not_implemented(self):
+        raise NotImplementedError('Not implemented in mock')
+
+class Spectrometer:
+    def __init__(self) -> None:
+        self.__creation_time = time.time()
+
+    def read_dark_signal(self, n_times: int):
+        pass
+
+    def load_calibration_data(self, path: str):
+        pass
+
+    def read_raw_spectrum(self, n_times: int):
+        __not_implemented()
+
+    def read_spectrum(self, n_times: int) -> Spectrum:
+        if n_times != 1:
+            __not_implemented('Only n_times=1 supported in mock')
+        t = time.time()
+        d = int(((t - self.__creation_time)/2)*10) % len(_data)
+        return _data[d]
+
+    def read_sepctre(self, module: int):
+        return _data[module]
