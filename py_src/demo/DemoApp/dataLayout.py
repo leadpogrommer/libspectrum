3bec6936e6efe590aff85cefda7f6145ed63f43e
Oleg Ivanov <o.ivanov@g.nsu.ru>
2022-12-12 23:35:13 +0700

    file saving exeptions processed

--- py_src/demo/DemoApp/dataLayout.py
+++ py_src/demo/DemoApp/dataLayout.py
@@ -22,15 +22,25 @@ class DataLayout(QBoxLayout):
         self._calculated_data = [("0", "0")]
         self._calculated_data_label = QtWidgets.QLabel()
         self.addWidget(self._calculated_data_label)
-
+        self._receiver = ""
+        self._source_model = "Unnamed source"
         self._pdf_check.clicked.connect(self._enable_button)
         self._csv_check.clicked.connect(self._enable_button)
 
     def _enable_button(self):
         self._save_button.setDisabled(not self._pdf_check.isChecked() and not self._csv_check.isChecked())
 
-    def additional_info_window(self):
-        pass
+    def set_source_model(self):
+        dialog = QtWidgets.QInputDialog()
+        name, ok = dialog.getText(self.mainWindow, "Source model", "Enter model:")
+        if ok:
+            self._source_model = name
+
+    def set_receiver(self):
+        dialog = QtWidgets.QInputDialog()
+        name, ok = dialog.getText(self.mainWindow, "Receiver", "Enter Receiver:")
+        if ok:
+            self._receiver = name
 
     def _save(self):
         if (self._spectrum == None):
@@ -43,12 +53,15 @@ class DataLayout(QBoxLayout):
         while filename == "" and ok:
             filename, ok = dialog.getText(self.mainWindow, "Save file", "Filename must not be empty")
         if ok:
-            if self._pdf_check.isChecked():
-                _pdf_savior = PdfSavior()
-                _pdf_savior.save_in_pdf(filename, self._calculated_data, self._spectrum)
-            if self._csv_check.isChecked():
-                _csv_savior = CsvSavior()
-                _csv_savior.save_in_csv(filename, self._calculated_data)
+            try:
+                if self._pdf_check.isChecked():
+                    _pdf_savior = PdfSavior(self._receiver,self._source_model)
+                    _pdf_savior.save_in_pdf(filename, self._calculated_data, self._spectrum)
+                if self._csv_check.isChecked():
+                    _csv_savior = CsvSavior()
+                    _csv_savior.save_in_csv(filename, self._calculated_data)
+            except Exception:
+                self.mainWindow.error_window(text="Error file save")
         self._pdf_check.setDisabled(False)
         self._csv_check.setDisabled(False)
 
--- py_src/demo/DemoApp/savior.py
+++ py_src/demo/DemoApp/savior.py
@@ -8,24 +8,35 @@ import csv
 
 
 class PdfSavior(FPDF):
-    def __init__(self):
+    def __init__(self, receiver: string = "", source_model: string = "unnamed"):
         super(PdfSavior, self).__init__()
+        self._receiver = receiver
+        self._source_model = source_model
+
+    def header(self) -> None:
+        self.set_y(15)
+        self.set_x(100)
+        self.set_font("helvetica", "B", 18)
+        self.cell(w=20, txt=self._source_model, align="C")
 
     def footer(self) -> None:
         self.set_y(-15)
         current_date = date.today()
-        self.cell(10, 15, str(current_date))
+        self.cell(w=50, txt=str(current_date))
+        self.set_x(-20)
+        self.cell(w=50, txt=self._receiver, align="LC")
 
     def save_in_pdf(self, filename: string, data, spectrum: Spectrum):
         self.add_page()
         self.set_font("helvetica", "B", 8)
         plt.plot(spectrum.wavelength, np.mean(spectrum.samples, axis=0))
         plt.savefig(filename + '.png')
-        self.image(filename + '.png', 5, 5, 100, 75)
+        self.image(name=filename + '.png', x=62.5, y=30, w=100, h=75)
         plt.clf()
-        x = 100
-        y = 150
-        self.cell(x, y, str(data))
+        self.set_y(120)
+        for i in data:
+            self.cell(w=10, txt=str(i[0]) + " " + str(i[1]), align="Left")
+            self.set_y(self.get_y() + 10)
         self.output(filename + '.pdf')
 
 
