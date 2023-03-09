f730505186e21ae89df046a0300f90e1b176088e
Oleg Ivanov <o.ivanov@g.nsu.ru>
2023-03-09 08:13:50 +0700

    filename inputs changed to file dialogs

--- py_src/demo/DemoApp/dataLayout.py
+++ py_src/demo/DemoApp/dataLayout.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QBoxLayout, QWidget
-from dataEstimator import DataEstimator
+from dataEstimator import DataEstimator, reshape_spectrum, cct
 from pyspectrum import Spectrum
 from savior import Saver
 
@@ -10,14 +10,11 @@ class DataLayout(QBoxLayout):
         super(QBoxLayout, self).__init__(QBoxLayout.Direction(3))
         self.mainWindow = parent
         self._estimator = DataEstimator()
-        self._save_button = QtWidgets.QPushButton("Save")
-        self._save_button.clicked.connect(self._save)
-        self.addWidget(self._save_button)
         self._spectrum = None
         self._calculated_data = [("0", "0")]
         self._calculated_data_label = QtWidgets.QLabel()
         self.addWidget(self._calculated_data_label)
-        self._savior = Saver("pdf")
+        self._savior = Saver()
 
     def set_source_model(self):
         dialog = QtWidgets.QInputDialog()
@@ -25,25 +22,15 @@ class DataLayout(QBoxLayout):
         if ok:
             self._savior.set_source_model(name)
 
-    def set_receiver(self):
-        dialog = QtWidgets.QInputDialog()
-        name, ok = dialog.getText(self.mainWindow, "Receiver", "Enter Receiver:")
-        if ok:
-            self._savior.set_receiver(name)
-
-    def _save(self):
+    def save(self,savetype:str):
         if self._spectrum is None:
             self.mainWindow.error_window(text="No spectrum registered")
             return
-        dialog = QtWidgets.QInputDialog()
-        filename, ok = dialog.getText(self.mainWindow, "Save file", "Enter a filename:")
-        while filename == "" and ok:
-            filename, ok = dialog.getText(self.mainWindow, "Save file", "Filename must not be empty")
-        if ok:
-            try:
-                self._savior.save(filename, self._spectrum, self._calculated_data)
-            except Exception:
-                self.mainWindow.error_window(text="Error file save")
+        dialog= QtWidgets.QFileDialog()
+        filename=dialog.getSaveFileName()
+        if (filename[0] !=""):
+            self._savior.save(filename[0], self._spectrum, self._calculated_data,savetype)
+
 
     def calculateData(self, spectrum: Spectrum):
         self._spectrum = spectrum
