bf9c75cc024a30a7b106e71475b30df57fa3de32
leadpogrommer <42062111+leadpogrommer@users.noreply.github.com>
2023-04-04 13:34:49 +0000

    Deploying to gh-pages from @ leadpogrommer/libspectrum@480aae690fc1540e6410108dc3bdbc1418487c0a 🚀

---
+++ examples/realtime_graph.py
@@ -0,0 +1,58 @@
+from pyspectrum import Spectrometer, usb_spectrometer, FactoryConfig
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.widgets import Button
+from os import path
+from time import time
+
+d = Spectrometer(usb_spectrometer(), FactoryConfig.default())
+
+matplotlib.use("Qt5agg")
+figure, axs = plt.subplots(ncols=2)
+ax = axs[0]
+
+figure.subplots_adjust(bottom=0.2)
+
+running = True
+
+
+def on_close(_):
+    global running
+    running = False
+
+
+def read_dark_signal(_):
+    d.read_dark_signal(10)
+
+
+figure.canvas.mpl_connect('close_event', on_close)
+
+ax_dark = figure.add_axes([0.5, 0.05, 0.2, 0.075])
+ax_profile = figure.add_axes([0.71, 0.05, 0.2, 0.075])
+
+b_dark = Button(ax_dark, 'Read dark signal')
+b_dark.on_clicked(read_dark_signal)
+
+b_profile = Button(ax_profile, 'Load profile data')
+profile_path = path.join(path.dirname(path.realpath(__file__)), 'data/profile.json')
+b_profile.on_clicked(lambda e: d.set_config(wavelength_calibration_path=profile_path))
+
+n_times = 1
+d.set_config(n_times=n_times, exposure=100)
+
+while running:
+    read_start = time()
+    if d.is_configured:
+        data = d.read()
+        wl = data.wavelength
+    else:
+        data = d.read_raw()
+        wl = np.array(range(0, data.n_numbers))
+    read_time = time() - read_start
+
+    ax.clear()
+    axs[1].clear()
+    ax.plot(wl, np.mean(data.intensity, axis=0))
+    axs[1].plot(wl, np.max(data.clipped, axis=0))
+    plt.pause(0.05)
---
+++ reference/reference.md
@@ -0,0 +1,22 @@
+---
+hide:
+- navigation
+---
+# Документация API
+
+## Spectrometer
+::: pyspectrum.Spectrometer
+
+## FactoryConfig
+::: pyspectrum.FactoryConfig
+
+## Data
+::: pyspectrum.Data
+
+## Spectrum
+::: pyspectrum.Spectrum
+    selection:
+        inherited_members: true
+
+## usb_spectrometer
+::: pyspectrum.usb_spectrometer
